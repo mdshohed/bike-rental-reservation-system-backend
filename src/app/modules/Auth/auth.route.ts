eb34a820cfcf80171452d0b0fd58dc6d98dc5e3f
mdshohed <mdshohed170@gmail.com>
2024-08-04 01:32:58 +0600

    file setup

---
+++ src/app/modules/Auth/auth.route.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post(
+  '/login',
+
+);
+
+router.post(
+  '/change-password',
+
+);
+
+router.post(
+  '/refresh-token',
+
+);
+
+export const AuthRoutes = router;

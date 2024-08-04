15663516593f4a57bcf15d8765b3410951d55877
mdshohed <mdshohed170@gmail.com>
2024-08-04 16:04:39 +0600

    .gitignore file delete

---
+++ dist/app/routes/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const auth_route_1 = require("../modules/Auth/auth.route");
+const router = (0, express_1.Router)();
+const moduleRoutes = [
+    {
+        path: '/users',
+        route: auth_route_1.AuthRoutes,
+    },
+];
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+exports.default = router;

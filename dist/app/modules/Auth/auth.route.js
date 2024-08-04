37379e0d6f411cf376cfdd8eafda4bda561df3de
mdshohed <mdshohed170@gmail.com>
2024-08-04 15:53:55 +0600

    updated

---
+++ dist/app/modules/Auth/auth.route.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthRoutes = void 0;
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
+let a = 10;
+console.log(router);
+router.post('/login');
+router.post('/change-password');
+router.post('/refresh-token');
+exports.AuthRoutes = router;
---
+++ src/app/modules/User/user.interface.ts
@@ -0,0 +1,11 @@
+
+
+export interface TUser {
+  id: string;
+  email: string;
+  password: string;
+  phone: boolean; 
+  passwordChangedAt?: Date;
+  role: "admin" | "user";
+  isDeleted: boolean; 
+}
